844905530e6a8b0f8c91a1d28c43a086c53a9036
Lauro Floresta <lauro.floresta@useargo.com>
2022-03-03 18:09:24 -0300

    first commit

---
+++ src/main/java/com/api/develcadastro/develcadastro/DevelCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.api.develcadastro.develcadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/develcadastro/develcadastro/DevelCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.develcadastro.develcadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
